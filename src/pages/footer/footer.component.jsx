e261ad58cb1cf2c22877888dd9b3ba9abde695c5
Dadi Rosadi <dadirosadi117@gmail.com>
2020-12-06 14:08:15 +0700

    feat(master) fix layouting product

--- src/pages/footer/footer.component.jsx
+++ src/pages/footer/footer.component.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import ImageFooter from '../../assets/footer.png';
+import ImageFooter from '../../assets/footer.jpg';
 import './footer.styles.scss';
 
 const FooterPage = () => (
     <div className='footer'>
-        <img className='image' alt="header" src={ImageFooter} />
-    </div>
+        <div>
+            <h2 className="name" >Dadi Rosadi</h2>
+            <h3 className="name">copyright@dadirosadi117</h3>
+            <p className="desc">Never regret a day in your life, good days give happiness, bad days give experiences, worst day give lessons, and best day give memories</p>
+        </div>
+        <div>
+            <img className="image" src={ImageFooter} alt="footer" />
+        </div>
+
+    </div >
 );
 
 export default FooterPage;
